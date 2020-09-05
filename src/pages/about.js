c42d66f86ddb5075b8e8d8e9fb0940fcc1034f95
Simon Richards <s.richards@dewynters.com>
2020-09-05 23:32:33 +0100

    latest

--- src/pages/about.js
+++ src/pages/about.js
@@ -14,7 +14,8 @@ const ContainerMaxStyled = styled(ContainerMax)`
 
 	p {
 		/* font-family: ${props => props.theme.font.family.satisfy}; */
-		font-size: ${props => props.theme.font.size.lg};
+		font-size: ${props => props.theme.font.size.base};
+		font-style: italic;
 	}
 `
 
@@ -24,6 +25,14 @@ const AboutPage = () => {
             aboutme: file(relativePath: { eq: "best-day-of-our-lives.jpg" }) {
 				childImageSharp {
 					fluid(maxWidth: 700, quality: 100) {
+						...GatsbyImageSharpFluid_withWebp_noBase64
+						...GatsbyImageSharpFluidLimitPresentationSize
+					}
+				}
+            }
+            about: file(relativePath: { eq: "about.jpg" }) {
+				childImageSharp {
+					fluid(maxWidth: 800, quality: 90) {
 						...GatsbyImageSharpFluid_withWebp_noBase64
 						...GatsbyImageSharpFluidLimitPresentationSize
 					}
@@ -43,64 +52,38 @@ const AboutPage = () => {
 			
 			</TextContainer>
 
-			<ContainerMaxStyled>
-				<Row>
-					<Col md={6}>
-						<img src="https://placehold.it/800x800" alt="" />
-					</Col>
-					<Col md={6}>
-						<Text>
-							Dear Visitors, 
-						</Text>
+			<ContainerMaxStyled className="pt-4">
 
-						<Text>
-							I look forward to being of assistance to you with your wedding here in Taormina. I just wanted to introduce myself and tell you a little bit about me, as I am a great believer that personal contact and first impressions are very important, particularly in this very special field.
-						</Text>
+				<Img fluid={data.about.childImageSharp.fluid} alt="" className="mb-4 " style={{ float: "left", width: "40%", maxHeight: "100%", margin: "0 2rem .5rem 0" }} />
 
-						<Text>
-							I was born and raised in London of Italian descent Italian. In my late teens, bored with my job in Barclays Bank, I got the travel bug and after a couple of worldwide trips eventually ended up in Sicily, where I permanently settled. Having lived here in Sicily for more than 30 years, I now consider myself to be a true "local" and I have acquired an excellent in-depth knowledge of this island and of Taormina in particular.
-						</Text>
+				<Text>
+					Dear Visitors, 
+				</Text>
 
-						<Text>
-							As well as being a legally registered Wedding & Events Planner, I am also a qualified Interpreter and a fully Licensed Tour Guide with a great passion for Sicily. My work experiences are wide and varied; ranging from Travel Agency work, organising and guiding Excursions and Tours, to the planning and organisation of large Conferences and Corporate Events. Before launching “Bouquets and Bows” in the Spring of 2004,  I was Regional Manager for Sicily for 8 years for a leading UK Specialist Tour Operator completely responsible for the running of the whole of the Sicilian side of their business. 
-						</Text>
+				<Text>
+					I look forward to being of assistance to you with your wedding here in Taormina. I just wanted to introduce myself and tell you a little bit about me, as I am a great believer that personal contact and first impressions are very important, particularly in this very special field.
+				</Text>
 
-						<Text>
-							With a broad portfolio of experience in Sicilian tourism and the travel industry behind me, Wedding and Event Planning became the logical enhancement of my career using my vast local knowledge in a very original and successful way, to organise many lovely weddings here in Taormina. I was actually one of the first wedding planners in the whole of Italy to offer destination weddings in this fabulous resort of Taormina. 
-						</Text>
-					</Col>
-				</Row>
-			</ContainerMaxStyled>
+				<Text>
+					I was born and raised in London of Italian descent Italian. In my late teens, bored with my job in Barclays Bank, I got the travel bug and after a couple of worldwide trips eventually ended up in Sicily, where I permanently settled. Having lived here in Sicily for more than 30 years, I now consider myself to be a true "local" and I have acquired an excellent in-depth knowledge of this island and of Taormina in particular.
+				</Text>
 
+				<Text>
+					As well as being a legally registered Wedding & Events Planner, I am also a qualified Interpreter and a fully Licensed Tour Guide with a great passion for Sicily. My work experiences are wide and varied; ranging from Travel Agency work, organising and guiding Excursions and Tours, to the planning and organisation of large Conferences and Corporate Events. Before launching “Bouquets and Bows” in the Spring of 2004,  I was Regional Manager for Sicily for 8 years for a leading UK Specialist Tour Operator completely responsible for the running of the whole of the Sicilian side of their business. 
+				</Text>
 
-			<ContainerMaxStyled>
-				<Row>
-					<Col md={6}>
-						<Text>
-							I am very much a “people” person, bubbly and lively and my enthusiasm is contagious. I am a real stickler for perfection and attention to detail and I repute myself to be highly professional with good interpersonal and organisational skills. 
-						</Text>
+				<Text>
+					With a broad portfolio of experience in Sicilian tourism and the travel industry behind me, Wedding and Event Planning became the logical enhancement of my career using my vast local knowledge in a very original and successful way, to organise many lovely weddings here in Taormina. I was actually one of the first wedding planners in the whole of Italy to offer destination weddings in this fabulous resort of Taormina. 
+				</Text>
 
-						<Text>
-							Every single wedding and each and every couple I work with are very special to me and are all treated with my utmost dedication. My goal is to always exceed expectations. Wedding Planning is not just a job to me it is my life. I am extremely passionate about both my profession and Taormina. I am sure you will get to love Sicily as much as I do, and find Taormina to be the perfect choice for your very special and unique Destination Wedding.
-						</Text>
-					</Col>
-					<Col md={6}>
-						<img src="https://placehold.it/800x500" alt="" />
-					</Col>
-				</Row>
-			</ContainerMaxStyled>
+				<Text>
+					I am very much a “people” person, bubbly and lively and my enthusiasm is contagious. I am a real stickler for perfection and attention to detail and I repute myself to be highly professional with good interpersonal and organisational skills. 
+				</Text>
 
-			{/* Best day o f lives phoho */}
+				<Text>
+					Every single wedding and each and every couple I work with are very special to me and are all treated with my utmost dedication. My goal is to always exceed expectations. Wedding Planning is not just a job to me it is my life. I am extremely passionate about both my profession and Taormina. I am sure you will get to love Sicily as much as I do, and find Taormina to be the perfect choice for your very special and unique Destination Wedding.
+				</Text>
 
-			<Container>
-				<Row className="justify-content-center">
-					<Col md={9}>
-						<Img fluid={data.aboutme.childImageSharp.fluid} alt="Best Day of Our Lives" className="mb-4 "/>
-					</Col>
-				</Row>
-			</Container>
-
-			<ContainerMaxStyled className="text-center">
 				<Text>
 					All of the photographs used throughout my website and on my social pages are all from weddings I have had the pleasure of organising here in the Taormina area. So a huge thank you to my wonderful photographers who have captured the true essence of Weddings in Taormina and to all of my lovely couples for allowing me use their beautiful photographs here and elsewhere. 
 				</Text>
@@ -121,11 +104,12 @@ const AboutPage = () => {
 					My kindest regards<br/>
 					Liz  
 				</Text>
-				
+			</ContainerMaxStyled>
+			<Container>
 				<Text md center cinzel primary xl>
 					Bouquets and Bows “Experience Shows”
 				</Text>
-			</ContainerMaxStyled>
+			</Container>
 		</Layout>
 	)
 }
